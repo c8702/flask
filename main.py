d5b24d3e586d56841d0024bcf41d7be6a2d53ea5
calpryce <8702@balcarras.gloucs.sch.uk>
2024-09-16 14:30:03 +0000

    built basic login&signup prototypes

---
+++ main.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = "qwertyuiop"
+
+##routing
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/signup")
+def signup():
+    return render_template("signup.html")
+
+
+
+######
+app.run(debug = True)
